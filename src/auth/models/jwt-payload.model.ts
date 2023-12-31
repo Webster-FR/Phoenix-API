d5035e2c6559bd32a9b077e2695a5963dcd4b6cd
Czekaj Tom <red57101@gmail.com>
2023-12-31 04:05:58 +0100

    :fire: Remove all unused code

--- src/auth/models/jwt-payload.model.ts
+++
@@ -1,7 +0,0 @@
-import * as jwt from "jsonwebtoken";
-import {ApiProperty} from "@nestjs/swagger";
-
-export class JwtPayloadModel implements jwt.JwtPayload{
-    @ApiProperty()
-        id: number;
-}
