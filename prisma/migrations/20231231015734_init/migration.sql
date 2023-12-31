3104e25e29cc9f1f663eb644d54d075048c78d1e
Czekaj Tom <red57101@gmail.com>
2023-12-31 04:10:54 +0100

    :sparkles: Add database structure

---
+++ prisma/migrations/20231231015734_init/migration.sql
@@ -0,0 +1,193 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+    "verification_code_id" INTEGER,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "users_verification_code_id_fkey" FOREIGN KEY ("verification_code_id") REFERENCES "verification_codes" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "verification_codes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "code" TEXT NOT NULL,
+    "iat" DATETIME NOT NULL,
+    "exp" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "tokens" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "sum" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "is_refresh" BOOLEAN NOT NULL,
+    "expires" DATETIME NOT NULL,
+    "blacklisted" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "deadline" DATETIME NOT NULL,
+    "parent_id" INTEGER,
+    "frequency" TEXT,
+    "icon" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "todos_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "todos_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "todos" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "tips" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tips" TEXT NOT NULL,
+    "author" TEXT,
+    "order" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "banks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "user_id" INTEGER,
+    CONSTRAINT "banks_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "bank_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    CONSTRAINT "accounts_bank_id_fkey" FOREIGN KEY ("bank_id") REFERENCES "banks" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "recurring_transactions" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "wording" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "next_occurrence" DATETIME NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "category_id" INTEGER,
+    "from_account_id" INTEGER,
+    "to_account_id" INTEGER,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "recurring_transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "recurring_transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "transaction_categories" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "recurring_transactions_from_account_id_fkey" FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "recurring_transactions_to_account_id_fkey" FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "transaction_categories" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    CONSTRAINT "transaction_categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "internal_ledger" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "account_id" INTEGER NOT NULL,
+    "debit" REAL,
+    "credit" REAL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "internal_ledger_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "accounts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "transaction_types" (
+    "ulid" TEXT NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "internal_transactions" (
+    "ulid" TEXT NOT NULL PRIMARY KEY,
+    "amount" REAL NOT NULL,
+    "wording" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "debit_internal_ledger_id" INTEGER NOT NULL,
+    "credit_internal_ledger_id" INTEGER NOT NULL,
+    "rectification_ulid" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "internal_transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "transaction_categories" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "internal_transactions_debit_internal_ledger_id_fkey" FOREIGN KEY ("debit_internal_ledger_id") REFERENCES "internal_ledger" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "internal_transactions_credit_internal_ledger_id_fkey" FOREIGN KEY ("credit_internal_ledger_id") REFERENCES "internal_ledger" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "internal_transactions_rectification_ulid_fkey" FOREIGN KEY ("rectification_ulid") REFERENCES "internal_transactions" ("ulid") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "expense_transactions" (
+    "ulid" TEXT NOT NULL PRIMARY KEY,
+    "amount" REAL NOT NULL,
+    "wording" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "internal_ledger_id" INTEGER NOT NULL,
+    "rectification_ulid" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "expense_transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "transaction_categories" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "expense_transactions_internal_ledger_id_fkey" FOREIGN KEY ("internal_ledger_id") REFERENCES "internal_ledger" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "expense_transactions_rectification_ulid_fkey" FOREIGN KEY ("rectification_ulid") REFERENCES "expense_transactions" ("ulid") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "income_transactions" (
+    "ulid" TEXT NOT NULL PRIMARY KEY,
+    "amount" REAL NOT NULL,
+    "wording" TEXT NOT NULL,
+    "category_id" INTEGER NOT NULL,
+    "internal_ledger_id" INTEGER NOT NULL,
+    "rectification_ulid" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "income_transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "transaction_categories" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "income_transactions_internal_ledger_id_fkey" FOREIGN KEY ("internal_ledger_id") REFERENCES "internal_ledger" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "income_transactions_rectification_ulid_fkey" FOREIGN KEY ("rectification_ulid") REFERENCES "income_transactions" ("ulid") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_verification_code_id_key" ON "users"("verification_code_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_codes_code_key" ON "verification_codes"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tokens_sum_key" ON "tokens"("sum");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tips_tips_key" ON "tips"("tips");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tips_order_key" ON "tips"("order");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "banks_name_key" ON "banks"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_name_key" ON "accounts"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "transaction_types_type_key" ON "transaction_types"("type");
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,125 @@
+// noinspection TypeScriptValidateJSTypes
+
 import {PrismaClient} from "@prisma/client";
+import {EncryptionService} from "../src/services/encryption.service";
 
 // initialize Prisma Client
 const prisma = new PrismaClient();
+const encryptionService = new EncryptionService();
 
 async function main(){
-    const defaultGroup = await prisma.group.upsert({
-        where: {name: "default"},
+    const testUser = await prisma.user.upsert({
+        where: {id: 1},
         update: {},
         create: {
-            name: "default",
+            id: 1,
+            username: "test",
+            email: "test@exemple.org",
+            password: await encryptionService.hashPassword("password"),
+            secret: encryptionService.generateSecret(),
+            verification_code_id: null,
+            created_at: new Date(),
+            updated_at: new Date(),
         },
     });
 
-    const adminGroup = await prisma.group.upsert({
-        where: {name: "admin"},
+    const test1Todos = await prisma.todos.upsert({
+        where: {id: 1},
         update: {},
         create: {
-            name: "admin",
+            id: 1,
+            user_id: 1,
+            name: "Test 1",
+            completed: false,
+            deadline: new Date(),
+            frequency: null,
+            icon: "none",
+            color: "none",
+            created_at: new Date(),
+            updated_at: new Date(),
+        },
+    });
+    const test2Todos = await prisma.todos.upsert({
+        where: {id: 2},
+        update: {},
+        create: {
+            id: 2,
+            user_id: 1,
+            name: "Test 2",
+            completed: false,
+            deadline: new Date(),
+            parent_id: 1,
+            frequency: null,
+            icon: "none",
+            color: "none",
+            created_at: new Date(),
+            updated_at: new Date(),
         },
     });
 
-    const admin = await prisma.user.upsert({
-        where: {username: "admin"},
+    const tips = [];
+    for (let i = 1; i <= 31; i++){
+        tips.push(await prisma.tips.upsert({
+            where: {id: i},
+            update: {},
+            create: {
+                id: i,
+                tips: `Tip ${i}`,
+                order: i,
+            },
+        }));
+    }
+
+    const defaultBank = await prisma.banks.upsert({
+        where: {id: 1},
         update: {},
         create: {
-            username: "admin",
-            password: "$argon2id$v=19$m=12,t=10,p=1$emxtN2RndHBvenAwMDAwMA$Ecz+bRSpVdiq9BMMJbOfAw",
-            group_id: adminGroup.id,
+            id: 1,
+            name: "Default bank",
         },
     });
 
-    console.log({defaultGroup, adminGroup, admin});
+    const userBank = await prisma.banks.upsert({
+        where: {id: 2},
+        update: {},
+        create: {
+            id: 2,
+            user_id: 1,
+            name: "User bank",
+        },
+    });
+
+    const userAccount = await prisma.accounts.upsert({
+        where: {id: 1},
+        update: {},
+        create: {
+            id: 1,
+            name: "User account",
+            amount: 0,
+            bank_id: 2,
+            user_id: 1,
+        },
+    });
+
+    const recurringTransaction = await prisma.recurringTransaction.upsert({
+        where: {id: 1},
+        update: {},
+        create: {
+            id: 1,
+            user_id: 1,
+            wording: "Test",
+            type: "expense",
+            amount: 10,
+            next_occurrence: new Date(),
+            frequency: "monthly",
+            from_account_id: 1,
+            created_at: new Date(),
+            updated_at: new Date(),
+        },
+    });
+
+    console.log(testUser, test1Todos, test2Todos, tips, defaultBank, userBank, userAccount, recurringTransaction);
+    console.log("Seeding done !");
 }
 
 main().catch((e) => {
