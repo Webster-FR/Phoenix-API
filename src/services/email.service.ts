d5d67f8f8cde2c9e9ff7203adc23a79154fb7e5f
Czekaj Tom <red57101@gmail.com>
2024-01-02 05:54:58 +0100

    :rotating_light: Fix linter issues

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -4,6 +4,7 @@ import {Injectable} from "@nestjs/common";
 @Injectable()
 export class EmailService{
     async sendConfirmationEmail(email: string, code: string): Promise<void>{
+        console.log("Sending confirmation email to", email, "with code", code);
         throw new Error("Not implemented");
     }
 }
