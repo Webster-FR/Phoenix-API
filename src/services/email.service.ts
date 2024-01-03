c241ddb44bd9f20804e0e03793a9011c44f40687
Czekaj Tom <red57101@gmail.com>
2024-01-03 01:06:06 +0100

    :sparkles: Implement email sending for account creation

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,10 +1,32 @@
 import {Injectable} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import * as nodemailer from "nodemailer";
 
 
 @Injectable()
 export class EmailService{
+
+    constructor(
+        private readonly configService: ConfigService,
+    ){}
+
     async sendConfirmationEmail(email: string, code: string): Promise<void>{
-        console.log("Sending confirmation email to", email, "with code", code);
-        throw new Error("Not implemented");
+        const emailUser = this.configService.get("EMAIL_USER");
+        const emailPassword = this.configService.get("EMAIL_PASSWORD");
+        const emailService = this.configService.get("EMAIL_SERVICE");
+        const transporter = nodemailer.createTransport({
+            service: emailService,
+            auth: {
+                user: emailUser,
+                pass: emailPassword,
+            },
+        });
+        const mailOptions = {
+            from: emailUser,
+            to: email,
+            subject: "[Phoenix] Confirm your account",
+            text: `Your confirmation code is ${code}`,
+        };
+        await transporter.sendMail(mailOptions);
     }
 }
