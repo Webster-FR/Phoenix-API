0272493655266d45fcffdf30ed1296285c7a006f
Czekaj Tom <red57101@gmail.com>
2024-01-05 21:16:27 +0100

    :rocket: Add verification code related methods

--- src/verification-codes/verification-codes.service.ts
+++ src/verification-codes/verification-codes.service.ts
@@ -1,20 +1,37 @@
 import {Injectable} from "@nestjs/common";
-import {VerificationCodeEntity} from "../users/models/entities/verification-code.entity";
+import {VerificationCodeEntity} from "./models/entities/verification-code.entity";
 import {PrismaService} from "../services/prisma.service";
+import {ConfigService} from "@nestjs/config";
+import {EncryptionService} from "../services/encryption.service";
 
 @Injectable()
 export class VerificationCodesService{
 
     constructor(
         private readonly prismaService: PrismaService,
+        private readonly configService: ConfigService,
+        private readonly encryptionService: EncryptionService,
     ){}
 
-    generateConfirmationCode(): string{
-        return String(Math.floor(100000 + Math.random() * 900000));
+    async checkCode(verificationCodeId: number, code: string): Promise<boolean>{
+        const verificationCode = await this.prismaService.verificationCodes.findUnique({where: {id: verificationCodeId}});
+        if(!verificationCode)
+            return false;
+        return await this.checkCodeValidity(verificationCodeId) && verificationCode.code === code;
+    }
+
+    async checkCodeValidity(verificationCodeId: number): Promise<boolean>{
+        const verificationCode: VerificationCodeEntity = await this.prismaService.verificationCodes.findUnique({where: {id: verificationCodeId}});
+        if (!verificationCode)
+            return false;
+        const now = new Date();
+        const iat = new Date(verificationCode.iat);
+        const validUntil = new Date(iat.getTime() + (this.configService.get<number>("VC_DURATION") * 60000));
+        return now <= validUntil;
     }
 
     async generateNewCode(verificationCodeId?: number): Promise<VerificationCodeEntity>{
-        const newCode = this.generateConfirmationCode();
+        const newCode = this.encryptionService.generateSecret();
         if(verificationCodeId){
             return this.prismaService.verificationCodes.update({
                 where: {
@@ -37,6 +54,10 @@ export class VerificationCodesService{
 
     async findById(id: number): Promise<VerificationCodeEntity>{
         return this.prismaService.verificationCodes.findUnique({where: {id: id}});
+    }
+
+    async findByCode(code: string): Promise<VerificationCodeEntity>{
+        return this.prismaService.verificationCodes.findUnique({where: {code: code}});
     }
 
     async deleteById(id: number): Promise<VerificationCodeEntity>{
