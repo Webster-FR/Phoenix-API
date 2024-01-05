7bfe1c8de20442c19030d55a5095d794d114db7f
Czekaj Tom <red57101@gmail.com>
2024-01-05 23:39:50 +0100

    :bug: Fix module imports

--- src/banks/banks.module.ts
+++ src/banks/banks.module.ts
@@ -1,9 +1,12 @@
 import {Module} from "@nestjs/common";
 import {BanksService} from "./banks.service";
 import {BanksController} from "./banks.controller";
+import {ServicesModule} from "../services/services.module";
+import {UsersModule} from "../users/users.module";
 
 @Module({
     controllers: [BanksController],
     providers: [BanksService],
+    imports: [ServicesModule, UsersModule],
 })
 export class BanksModule{}
