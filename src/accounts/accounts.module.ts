6dbc45767661eddcdffbce0f510a61bbd096c56e
Czekaj Tom <red57101@gmail.com>
2024-01-06 03:17:43 +0100

    :bug: Fix module dependency bug

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,12 @@
 import {Module} from "@nestjs/common";
 import {AccountsService} from "./accounts.service";
 import {AccountsController} from "./accounts.controller";
+import {ServicesModule} from "../services/services.module";
+import {UsersModule} from "../users/users.module";
 
 @Module({
     controllers: [AccountsController],
     providers: [AccountsService],
+    imports: [ServicesModule, UsersModule],
 })
 export class AccountsModule{}
