86b60cc625ca595fb09b584e9c307fb9a37879f1
Czekaj Tom <red57101@gmail.com>
2024-01-06 04:52:57 +0100

    :memo: Add accounts documentation

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,6 +1,6 @@
-import {Body, Controller, Delete, Get, Param, Patch, Post, Req, UseGuards} from "@nestjs/common";
+import {Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Req, UseGuards} from "@nestjs/common";
 import {AccountsService} from "./accounts.service";
-import {ApiBearerAuth, ApiTags} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {MaintenanceGuard} from "../maintenance/guards/maintenance.guard";
 import {AtGuard} from "../auth/guards/at.guard";
 import {AccountEntity} from "./models/entities/account.entity";
@@ -20,6 +20,9 @@ export class AccountsController{
     @Get()
     @UseGuards(AtGuard)
     @ApiBearerAuth()
+    @ApiResponse({status: HttpStatus.OK, description: "Get all accounts", type: AccountEntity, isArray: true})
+    @ApiResponse({status: HttpStatus.UNAUTHORIZED, description: "Invalid or missing access token"})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, description: "User not found"})
     async getAccounts(@Req() req: any): Promise<AccountEntity[]>{
         return this.accountsService.getAccounts(req.user.id);
     }
@@ -27,6 +30,11 @@ export class AccountsController{
     @Post()
     @UseGuards(AtGuard)
     @ApiBearerAuth()
+    @ApiResponse({status: HttpStatus.CREATED, description: "Create an account", type: AccountEntity})
+    @ApiResponse({status: HttpStatus.UNAUTHORIZED, description: "Invalid or missing access token"})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, description: "User not found"})
+    @ApiResponse({status: HttpStatus.CONFLICT, description: "Account already exists"})
+    @ApiResponse({status: HttpStatus.BAD_REQUEST, description: "Invalid or missing fields"})
     async createAccount(@Req() req: any, @Body() createAccountDto: CreateAccountDto): Promise<AccountEntity>{
         return this.accountsService.createAccount(req.user.id, createAccountDto.name, createAccountDto.amount, createAccountDto.bank_id);
     }
@@ -34,6 +42,10 @@ export class AccountsController{
     @Patch(":id/name")
     @UseGuards(AtGuard)
     @ApiBearerAuth()
+    @ApiResponse({status: HttpStatus.OK, description: "Update account name", type: AccountEntity})
+    @ApiResponse({status: HttpStatus.UNAUTHORIZED, description: "Invalid or missing access token"})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, description: "User or account not found"})
+    @ApiResponse({status: HttpStatus.BAD_REQUEST, description: "Invalid or missing fields"})
     async updateAccountName(@Req() req: any, @Param() idDto: IdDto, @Body() renameAccountDto: RenameAccountDto): Promise<AccountEntity>{
         return this.accountsService.updateAccountName(req.user.id, idDto.id, renameAccountDto.name);
     }
@@ -41,6 +53,10 @@ export class AccountsController{
     @Delete(":id")
     @UseGuards(AtGuard)
     @ApiBearerAuth()
+    @ApiResponse({status: HttpStatus.OK, description: "Delete an account", type: AccountEntity})
+    @ApiResponse({status: HttpStatus.UNAUTHORIZED, description: "Invalid or missing access token"})
+    @ApiResponse({status: HttpStatus.NOT_FOUND, description: "User or account not found"})
+    @ApiResponse({status: HttpStatus.BAD_REQUEST, description: "Invalid or missing id"})
     async deleteAccount(@Req() req: any, @Param() idDto: IdDto): Promise<AccountEntity>{
         return this.accountsService.deleteAccount(req.user.id, idDto.id);
     }
