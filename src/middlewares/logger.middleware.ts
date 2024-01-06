8fa69802e1feb527125013741c87ceba01b6eea1
Czekaj Tom <red57101@gmail.com>
2024-01-06 06:36:41 +0100

    :loud_sound: Make logging middleware use nestjs middleware

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,27 +1,21 @@
-import {Injectable, NestMiddleware} from "@nestjs/common";
+import {Injectable, Logger, NestMiddleware} from "@nestjs/common";
 import {FastifyReply, FastifyRequest} from "fastify";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware{
-    private static getCurrentTime(): string{
-        const now = new Date();
-        const hours = String(now.getHours()).padStart(2, "0");
-        const minutes = String(now.getMinutes()).padStart(2, "0");
-        const seconds = String(now.getSeconds()).padStart(2, "0");
-        return `[${hours}:${minutes}:${seconds}]`;
-    }
+
+    static logger: Logger = new Logger(LoggerMiddleware.name);
 
     use(req: FastifyRequest["raw"], res: FastifyReply["raw"], next: () => void){
         const startTime = Date.now();
         res.on("finish", () => {
-            const currentTime = LoggerMiddleware.getCurrentTime();
             const httpOrHttps = req.connection.localPort.toString() === process.env.HTTPS_PORT ? "HTTPS" : "HTTP";
             const method = req.method;
             const path = req.url;
             const statusCode = res.statusCode;
             const duration = Date.now() - startTime;
             const resSize = res.getHeader("Content-Length") || "N/A";
-            console.log(`${currentTime} ${httpOrHttps} ${method} ${path} ${statusCode} ${duration}ms ${resSize}`);
+            LoggerMiddleware.logger.log(`${httpOrHttps} ${method} ${path} ${statusCode} ${duration}ms ${resSize}`);
         });
         next();
     }
