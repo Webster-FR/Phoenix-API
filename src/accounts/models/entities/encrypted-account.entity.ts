6736eff7233e5f3cff1d848956f10fd3b567735c
Czekaj Tom <red57101@gmail.com>
2024-01-06 04:41:09 +0100

    :rocket: Add 2 accounts entities for database and api usage

--- src/accounts/models/entities/encrypted-account.entity.ts
+++ src/accounts/models/entities/encrypted-account.entity.ts
@@ -1,14 +1,15 @@
+import {Accounts} from "@prisma/client";
 import {ApiProperty} from "@nestjs/swagger";
 
-export class AccountEntity{
-    @ApiProperty()
-        id: number;
+export class EncryptedAccountEntity implements Accounts{
     @ApiProperty()
         name: string;
     @ApiProperty()
-        amount: number;
+        amount: string;
     @ApiProperty()
         bank_id: number;
     @ApiProperty()
         user_id: number;
+    @ApiProperty()
+        id: number;
 }
