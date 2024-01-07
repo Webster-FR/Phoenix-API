591e58df44aba892f71d5d41101555aa6cf9f11c
Czekaj Tom <red57101@gmail.com>
2024-01-07 21:20:08 +0100

    :bug: Try fixing database seed

--- prisma/seeds/banks.seed.ts
+++ prisma/seeds/banks.seed.ts
@@ -1,8 +1,13 @@
 import {EncryptionService} from "../../src/services/encryption.service";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const encryptionService = new EncryptionService();
 const encryptionKey = process.env.SYMMETRIC_ENCRYPTION_KEY;
 const banksEncryptionStrength = parseInt(process.env.BANKS_ENCRYPTION_STRENGTH);
+
+console.log(encryptionKey, banksEncryptionStrength);
 
 export default [
     {
