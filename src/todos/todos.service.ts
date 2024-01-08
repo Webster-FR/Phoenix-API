3cf41ac693f751332aae3d725568ef7c543fabde
Czekaj Tom <red57101@gmail.com>
2024-01-08 02:26:57 +0100

    :rotating_light: Fix linter errors

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,4 @@
-import {BadRequestException, Injectable, NotFoundException} from "@nestjs/common";
+import {Injectable, NotFoundException} from "@nestjs/common";
 import {PrismaService} from "../services/prisma.service";
 import {TodoEntity} from "./models/entities/todo.entity";
 import {UsersService} from "../users/users.service";
