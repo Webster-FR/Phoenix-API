b448a9edf577329020152730cd4008b28428b654
Czekaj Tom <red57101@gmail.com>
2024-01-11 01:43:21 +0100

    :bug: Todo deadline are now nullable

--- src/todos/models/dto/create-todo.dto.ts
+++ src/todos/models/dto/create-todo.dto.ts
@@ -1,12 +1,11 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsDateString, IsNotEmpty} from "class-validator";
+import {IsNotEmpty} from "class-validator";
 
 export class CreateTodoDto{
     @ApiProperty()
     @IsNotEmpty()
         name: string;
     @ApiProperty()
-    @IsDateString()
         deadline: Date;
     @ApiProperty()
         parent_id: number;
