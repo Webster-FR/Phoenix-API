6bb0827a87426b9cc3ee7fe3517d15e4a27a048a
Czekaj Tom <red57101@gmail.com>
2024-01-11 20:47:12 +0100

    :sparkles: Add setBanks method

--- src/modules/cache/bank-cache.service.ts
+++ src/modules/cache/bank-cache.service.ts
@@ -35,59 +35,42 @@ export class BankCacheService{
         return null;
     }
 
+    async setBanks(user: UserEntity, banks: BankEntity[]): Promise<void>{
+        for(const bank of banks)
+            if(bank.user_id === user.id){
+                const userBanks: BankEntity[] = banks.filter(b => b.user_id === user.id);
+                await this.cacheManager.set(`banks-${user.id}`, userBanks, 0);
+            }else{
+                const defaultBanks: BankEntity[] = banks.filter(b => b.user_id === null);
+                await this.cacheManager.set("banks-null", defaultBanks, 0);
+            }
+    }
+
     async addBank(user: UserEntity, bank: BankEntity): Promise<void>{
-        if(bank.user_id){
-            const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
-            if(!banks)
-                await this.cacheManager.set(`banks-${user.id}`, [bank], 0);
-            else
-                await this.cacheManager.set(`banks-${user.id}`, banks.concat(bank), 0);
-        }else{
-            const banks: BankEntity[] = await this.cacheManager.get("banks-null");
-            if(!banks)
-                await this.cacheManager.set("banks-null", [bank], 0);
-            else
-                await this.cacheManager.set("banks-null", banks.concat(bank), 0);
-        }
+        const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
+        if(!banks)
+            await this.cacheManager.set(`banks-${user.id}`, [bank], 0);
+        else
+            await this.cacheManager.set(`banks-${user.id}`, banks.concat(bank), 0);
     }
 
     async updateBank(user: UserEntity, bank: BankEntity): Promise<void>{
-        if(bank.user_id){
-            const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
-            for(let i = 0; i < banks.length; i++)
-                if(banks[i].id === bank.id){
-                    banks[i] = bank;
-                    await this.cacheManager.set(`banks-${user.id}`, banks, 0);
-                    return;
-                }
-        }else{
-            const banks: BankEntity[] = await this.cacheManager.get("banks-null");
-            for(let i = 0; i < banks.length; i++)
-                if(banks[i].id === bank.id){
-                    banks[i] = bank;
-                    await this.cacheManager.set("banks-null", banks, 0);
-                    return;
-                }
-        }
+        const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
+        for(let i = 0; i < banks.length; i++)
+            if(banks[i].id === bank.id){
+                banks[i] = bank;
+                await this.cacheManager.set(`banks-${user.id}`, banks, 0);
+                return;
+            }
     }
 
     async deleteBank(user: UserEntity, bank: BankEntity): Promise<void>{
-        if(bank.user_id){
-            const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
-            for(let i = 0; i < banks.length; i++)
-                if(banks[i].id === bank.id){
-                    banks.splice(i, 1);
-                    await this.cacheManager.set(`banks-${user.id}`, banks, 0);
-                    return;
-                }
-        }else{
-            const banks: BankEntity[] = await this.cacheManager.get("banks-null");
-            for(let i = 0; i < banks.length; i++)
-                if(banks[i].id === bank.id){
-                    banks.splice(i, 1);
-                    await this.cacheManager.set("banks-null", banks, 0);
-                    return;
-                }
-        }
+        const banks: BankEntity[] = await this.cacheManager.get(`banks-${user.id}`);
+        for(let i = 0; i < banks.length; i++)
+            if(banks[i].id === bank.id){
+                banks.splice(i, 1);
+                await this.cacheManager.set(`banks-${user.id}`, banks, 0);
+                return;
+            }
     }
 }
