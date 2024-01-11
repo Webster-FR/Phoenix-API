11c0f44fd8077b5b9bcfa432313838da3c23f370
Czekaj Tom <red57101@gmail.com>
2024-01-11 03:57:21 +0100

    :art: Add accounting module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,9 @@ import {ConfigModule} from "@nestjs/config";
 import {UsersModule} from "./modules/security/users/users.module";
 import {VerificationCodesModule} from "./modules/security/verification-codes/verification-codes.module";
 import {TodosModule} from "./modules/todos/todos.module";
-import {BanksModule} from "./modules/accounting/banks/banks.module";
 import {ScheduleModule} from "@nestjs/schedule";
 import {TasksModule} from "./modules/tasks/tasks.module";
 import {SecretsModule} from "./modules/security/secrets/secrets.module";
-import {AccountsModule} from "./modules/accounting/accounts/accounts.module";
-import {LedgersModule} from "./modules/accounting/ledgers/ledgers.module";
-import {TransactionsModule} from "./modules/accounting/transactions/transactions.module";
-import {TransactionCategoriesModule} from "./modules/accounting/transaction-categories/transaction-categories.module";
 import {CacheModule} from "@nestjs/cache-manager";
 import {TokenCacheService} from "./modules/cache/token-cache.service";
 import {CacheModule as InternalCacheModule} from "./modules/cache/cache.module";
@@ -19,6 +14,7 @@ import {ServicesModule} from "./common/services/services.module";
 import type {RedisClientOptions} from "redis";
 import * as redisStore from "cache-manager-redis-store";
 import {MiscModule} from "./modules/misc/misc.module";
+import {AccountingModule} from "./modules/accounting/accounting.module";
 
 const redisUrl = process.env.REDIS_URL;
 const redisPassword = process.env.REDIS_PASSWORD;
@@ -37,16 +33,12 @@ const redisPassword = process.env.REDIS_PASSWORD;
         UsersModule,
         VerificationCodesModule,
         TodosModule,
-        BanksModule,
         TasksModule,
         SecretsModule,
-        AccountsModule,
-        LedgersModule,
-        TransactionsModule,
-        TransactionCategoriesModule,
         InternalCacheModule,
         ServicesModule,
-        MiscModule
+        MiscModule,
+        AccountingModule
     ],
     providers: [TokenCacheService],
 })
