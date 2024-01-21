dd4ed7c262bb01df6a1f5fb397cc5074d4b5a2b3
Czekaj Tom <red57101@gmail.com>
2024-01-21 16:39:46 +0100

    :seedling: Update seeding for the new user model

--- prisma/seeds/users.seed.ts
+++ prisma/seeds/users.seed.ts
@@ -11,8 +11,9 @@ const usersEncryptionStrength = parseInt(process.env.USERS_ENCRYPTION_STRENGTH);
 export default async(userSecret: string) => [
     {
         username: encryptionService.encryptSymmetric("test", userSecret, usersEncryptionStrength),
-        email: "test@exemple.org",
         password: await encryptionService.hash("wtviE6AZ!Ypf52pMetSR"),
+        email: encryptionService.encryptSymmetric("test@example.org", userSecret, usersEncryptionStrength),
+        email_sum: encryptionService.getSum("test@example.org").substring(0, 10),
         secret: encryptionService.encryptSymmetric(userSecret, encryptionKey, userSecretsEncryptionStrength),
         created_at: new Date(),
         updated_at: new Date(),
