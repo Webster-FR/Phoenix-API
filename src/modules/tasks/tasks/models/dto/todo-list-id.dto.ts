abcddbe72acc73181dc36ee4af44ab3fa298a5b6
Xen0Xys <red57101@gmail.com>
2024-02-07 17:37:22 +0100

    :ambulance: Fix invalid data casting

--- src/modules/tasks/tasks/models/dto/todo-list-id.dto.ts
+++ src/modules/tasks/tasks/models/dto/todo-list-id.dto.ts
@@ -1,10 +1,9 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsNotEmpty} from "class-validator";
-import {Type} from "class-transformer";
+import {IsInt, IsNotEmpty} from "class-validator";
 
 export class TodoListIdDto{
     @ApiProperty()
     @IsNotEmpty()
-    @Type(() => Number)
+    @IsInt()
         todo_list_id: number;
 }
